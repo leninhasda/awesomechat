3c6a265a469be29541805a722433ce3af6539d48
Lenin Hasda <leninhasda@gmail.com>
2016-10-10 23:48:57 +0600

    updated design

--- app.js
+++ app.js
@@ -56,4 +56,12 @@ app.use(function(err, req, res, next) {
 });
 
 
-module.exports = app;
+var debug = require('debug')('my-application');
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  debug('Express server listening on port ' + server.address().port);
+});
+
+// module.exports = app;
